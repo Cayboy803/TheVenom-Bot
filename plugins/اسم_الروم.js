1ec5f9dfcd0219911244cd184056e4a13c34a9d0
Cayboy803 <mm3877464@gmail.com>
2024-07-30 17:56:32 +0300

    Create اسم_الروم.js

---
+++ plugins/اسم_الروم.js
@@ -0,0 +1,15 @@
+var handler = async (m, { conn, text, isROwner, isOwner }) => {
+
+if (!text) return conn.reply(m.chat, 'هذا الأمر خاص بتغيير إسم المجموعة تكتب هكذا \n\n*.setgoupename* BOBIZA BOT GROUPE', m,  )
+await conn.groupUpdateSubject(m.chat, text)
+conn.reply(m.chat, `🚩 ${text ? `${text} *هو الاسم الجديد لهذه المجموعة*\n` : '*لم يعطوا اسما*'}`, m,  )
+}
+handler.help = ['اسم_الروم']
+handler.tags = ['المطور_فقط']
+handler.command = /^اسم_الروم$/i
+
+handler.botAdmin = true
+handler.group = true
+handler.admin = true
+
+export default handler
